826ab3f4325d10b0779f65cb3a6d0664fa02ed57
angelozerr <angelozerr@gmail.com>
2016-10-14 23:27:59 +0200

    Check if launch exists.

--- ts.eclipse.ide.angular2.cli/src/ts/eclipse/ide/angular2/internal/cli/launch/AngularCLILaunchHelper.java
+++ ts.eclipse.ide.angular2.cli/src/ts/eclipse/ide/angular2/internal/cli/launch/AngularCLILaunchHelper.java
@@ -1,3 +1,13 @@
+/**
+ *  Copyright (c) 2015-2016 Angelo ZERR.
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ *
+ *  Contributors:
+ *  Angelo Zerr <angelo.zerr@gmail.com> - initial API and implementation
+ */
 package ts.eclipse.ide.angular2.internal.cli.launch;
 
 import java.io.File;
@@ -5,6 +15,7 @@ import java.io.File;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.debug.core.DebugPlugin;
+import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.debug.core.ILaunchConfigurationType;
 import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
 import org.eclipse.debug.core.ILaunchManager;
@@ -13,9 +24,13 @@ import org.eclipse.debug.ui.DebugUITools;
 import ts.eclipse.ide.angular2.cli.NgCommand;
 import ts.eclipse.ide.angular2.cli.launch.AngularCLILaunchConstants;
 import ts.eclipse.ide.angular2.internal.cli.AngularCLIProject;
-import ts.eclipse.ide.angular2.internal.cli.Trace;
+import ts.eclipse.ide.core.TypeScriptCorePlugin;
 import ts.utils.FileUtils;
 
+/**
+ * ng launch helper.
+ *
+ */
 public class AngularCLILaunchHelper {
 
 	public static void launch(NgCommand ngCommand, IProject project) throws CoreException {
@@ -23,28 +38,57 @@ public class AngularCLILaunchHelper {
 	}
 
 	public static void launch(NgCommand ngCommand, IProject project, String mode) throws CoreException {
-		try {
-			ILaunchConfigurationWorkingCopy newConfiguration = createEmptyLaunchConfiguration();
+		String workingDir = project.getLocation().toString();
+		String operation = ngCommand.name().toLowerCase();
+
+		// Check if configuration already exists
+		ILaunchConfiguration ngConfiguration = chooseLaunchConfiguration(workingDir, operation);
+		if (ngConfiguration != null) {
+			ILaunchConfigurationWorkingCopy wc = ngConfiguration.getWorkingCopy();
+			ngConfiguration = wc.doSave();
+			DebugUITools.launch(ngConfiguration, mode);
+		} else {
+			// Creating Launch Configuration from scratch
+			ILaunchConfigurationWorkingCopy newConfiguration = createEmptyLaunchConfiguration(project.getName(),
+					operation);
 			File ngFile = AngularCLIProject.getAngularCLIProject(project).getSettings().getNgFile();
 			if (ngFile != null) {
 				newConfiguration.setAttribute(AngularCLILaunchConstants.NG_FILE_PATH, FileUtils.getPath(ngFile));
 			}
-			newConfiguration.setAttribute(AngularCLILaunchConstants.WORKING_DIR, project.getLocation().toString());
-			newConfiguration.setAttribute(AngularCLILaunchConstants.OPERATION, ngCommand.name());
+			newConfiguration.setAttribute(AngularCLILaunchConstants.WORKING_DIR, workingDir);
+			newConfiguration.setAttribute(AngularCLILaunchConstants.OPERATION, operation);
 			// newConfiguration.setAttribute(AngularCLILaunchConstants.OPERATION_PARAMETERS,
 			// "--live-reload-port 65535");
+			newConfiguration.doSave();
 			DebugUITools.launch(newConfiguration, mode);
-		} catch (CoreException e) {
-			Trace.trace(Trace.SEVERE, e.getLocalizedMessage());
 		}
 	}
 
-	private static ILaunchConfigurationWorkingCopy createEmptyLaunchConfiguration() throws CoreException {
+	private static ILaunchConfiguration chooseLaunchConfiguration(String workingDir, String operation) {
+		try {
+			ILaunchConfigurationType ngLaunchConfigurationType = DebugPlugin.getDefault().getLaunchManager()
+					.getLaunchConfigurationType(AngularCLILaunchConstants.LAUNCH_CONFIGURATION_ID);
+			ILaunchConfiguration[] ngConfigurations = DebugPlugin.getDefault().getLaunchManager()
+					.getLaunchConfigurations(ngLaunchConfigurationType);
+			for (ILaunchConfiguration conf : ngConfigurations) {
+				if (workingDir.equals(conf.getAttribute(AngularCLILaunchConstants.WORKING_DIR, (String) null))
+						&& operation.equals(conf.getAttribute(AngularCLILaunchConstants.OPERATION, (String) null))) {
+					return conf;
+				}
+			}
+		} catch (CoreException e) {
+			TypeScriptCorePlugin.logError(e, e.getMessage());
+		}
+		return null;
+	}
+
+	private static ILaunchConfigurationWorkingCopy createEmptyLaunchConfiguration(String projectName, String operation)
+			throws CoreException {
 		ILaunchManager launchManager = DebugPlugin.getDefault().getLaunchManager();
 		ILaunchConfigurationType launchConfigurationType = launchManager
 				.getLaunchConfigurationType(AngularCLILaunchConstants.LAUNCH_CONFIGURATION_ID);
 		ILaunchConfigurationWorkingCopy launchConfiguration = launchConfigurationType.newInstance(null,
-				launchManager.generateLaunchConfigurationName("angular-cli"));
+				launchManager.generateLaunchConfigurationName(projectName + " [" + operation + "]"));
 		return launchConfiguration;
 	}
 }

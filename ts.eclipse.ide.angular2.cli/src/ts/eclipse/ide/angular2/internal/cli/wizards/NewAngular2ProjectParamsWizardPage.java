d163d5e201590c58e19b93118be272764064ca7c
Robert <pattis_r@hotmail.de>
2017-02-01 20:09:23 +0100

    Add missing licence and autor Add comment to NewAngular2ProjectParamsWizardPage

--- ts.eclipse.ide.angular2.cli/src/ts/eclipse/ide/angular2/internal/cli/wizards/NewAngular2ProjectParamsWizardPage.java
+++ ts.eclipse.ide.angular2.cli/src/ts/eclipse/ide/angular2/internal/cli/wizards/NewAngular2ProjectParamsWizardPage.java
@@ -1,3 +1,14 @@
+/**
+ *  Copyright (c) 2015-2016 Angelo ZERR.
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ *
+ *  Contributors:
+ *  Springrbua - initial implementation
+ *  
+ */
 package ts.eclipse.ide.angular2.internal.cli.wizards;
 
 import org.eclipse.jface.wizard.WizardPage;
@@ -17,6 +28,10 @@ import org.eclipse.swt.widgets.Text;
 
 import ts.eclipse.ide.angular2.internal.cli.AngularCLIMessages;
 
+/**
+ * Wizard page for the optional Project-Parameters.
+ *
+ */
 public class NewAngular2ProjectParamsWizardPage extends WizardPage implements Listener {
 
 	private static final String PAGE_NAME	= "newProjectParamsPage";
